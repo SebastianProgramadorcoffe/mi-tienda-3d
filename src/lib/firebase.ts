35334d14e172aea4f29516c67e2f2c1c211a5b1b
inicioprogramadorcoffe <inicioprogramador216@gmail.com>
2026-04-25 00:36:21 -0500

    Solucionando problemas de seguridad

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJzZe_2ozJ3xLcXgiGNTJfjInr2DDiBsc",
-  authDomain: "tienda-productos-aura-esencia.firebaseapp.com",
-  projectId: "tienda-productos-aura-esencia",
-  storageBucket: "tienda-productos-aura-esencia.firebasestorage.app",
-  messagingSenderId: "591466725758",
-  appId: "1:591466725758:web:7e49cece777946dd52b32f",
+  apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:        process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
